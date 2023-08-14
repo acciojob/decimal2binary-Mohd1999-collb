8ae36f4de53bfe85fce7689bba0fe27d54054623
Mohd Talib <96992202+Mohd1999-collb@users.noreply.github.com>
2023-08-15 00:53:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function decimalToBinary(num) {
   //Write you code here
-  
+    let binary = [], i = 0;
+    while(num != 0){
+        let quotiont = num % 2;
+		binary[i++] = quotiont;
+		 num = parseInt(num / 2);
+    }
+ return binary.reverse().join("");  
 }
 
 window.decimalToBinary = decimalToBinary;
